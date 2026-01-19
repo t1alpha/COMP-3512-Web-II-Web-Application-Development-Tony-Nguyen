be8e4f51dda0195a820ddb351bea02ace272fdde
Tony Nguyen <91648659+t1alpha@users.noreply.github.com>
2026-01-18 18:32:34 -0700

    Exercise 2 – echo image

--- index.php
+++ index.php
@@ -1,42 +1,12 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <title>Registration Form</title>
-</head>
-<body>
+<?php
+$filename = "happy.jpg";
+$alt = "happy life";
+$classname = "box";
 
-<h1>Registration Form</h1>
+/* concatenation */
+echo "<img src='images/" . $filename . "' alt='" . $alt . "' class='" . $classname . "'>";
 
-<form action="process.php" method="POST">
+echo "<br><br>";
 
-  <label>First Name</label><br>
-  <input type="text" name="firstName" required><br><br>
-
-  <label>Last Name</label><br>
-  <input type="text" name="lastName" required><br><br>
-
-  <label>Email</label><br>
-  <input type="email" name="email" required><br><br>
-
-  <label>Password</label><br>
-  <input type="password" name="password" required><br><br>
-
-  <label>Program</label><br>
-  <select name="program" required>
-    <option value="">Select</option>
-    <option value="CIS">CIS</option>
-    <option value="CS">CS</option>
-    <option value="Web">Web</option>
-  </select><br><br>
-
-  <label>Study Mode</label><br>
-  <input type="radio" name="studyMode" value="Full-time" required> Full-time<br>
-  <input type="radio" name="studyMode" value="Part-time"> Part-time<br><br>
-
-  <button type="submit">Register</button>
-
-</form>
-
-</body>
-</html>
+/* embedded variables */
+echo "<img src='images/$filename' alt='$alt' class='$classname'>";
